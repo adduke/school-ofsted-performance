209b4c3af561da9ce624033f4543c80d21283a14
abiibrahim <adduke.ikr@gmail.com>
2024-06-12 14:33:27 +0100

    recent commits from pycharm

---
+++ School Ofsted performnce.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+st.write('hello world')
+x = st.text_input("What's your favourite movie?")
+st.write(f"favourite movie is {x}")
+pd.DataFrame()
+np.mean()
+sns.plot()
+plt.plot()
